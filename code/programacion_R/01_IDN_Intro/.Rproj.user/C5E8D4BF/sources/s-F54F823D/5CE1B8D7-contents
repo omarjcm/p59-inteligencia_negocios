a0dd0ae94788daabe5e25badcb88863cc45fe873
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2022-01-18 20:39:10 -0500

    Creacion de proyecto

---
+++ code/programacion_R/01_IDN_Intro/.Rproj.user/C5E8D4BF/sources/s-F54F823D/5CE1B8D7-contents
@@ -0,0 +1,6 @@
+library(readxl)
+
+data_banco <- read_excel("Data/Data_Banco.xlsx", sheet = "Data")
+data_sucursal <- read_excel("Data/Data_Banco.xlsx", sheet = "Data_Sucursal")
+data_cajero <- read_excel("Data/Data_Banco.xlsx", sheet = "Data_Cajero")
+
---
+++ code/programacion_R/01_IDN_Intro/Scripts/01_taller.R
@@ -0,0 +1,23 @@
+5+5
+
+a <- 5+5
+
+2 + 3 * 5
+
+log((1+2+3)/4)
+pi
+
+# Generar secuencias, repeticiones y aleatorios
+
+1:10
+seq(from=0, to=20, by=5)
+seq(from=5, by=5, length.out=10)
+rep(x=3, times='10')
+runif(n=100, min=1, max=5)
+
+rnorm(n=100, mean=100, sd=10)
+
+
+
+
+
