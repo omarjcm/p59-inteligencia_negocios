cce2e4dacb11abc575750106533a23b3269fbb36
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2022-01-25 20:54:15 -0500

    Manipulacion de datos, filter, select, arrange, cor

--- code/programacion_R/01_IDN_Intro/Scripts/02_taller.R
+++ code/programacion_R/01_IDN_Intro/Scripts/02_taller.R
@@ -1,6 +1,7 @@
 library(readxl)
 library(dplyr)
 library(tidyverse)
+library(magrittr)
 
 data_banco <- read_excel("Data/Data_Banco.xlsx", sheet = "Data")
 data_sucursal <- read_excel("Data/Data_Banco.xlsx", sheet = "Data_Sucursal")
@@ -40,4 +41,70 @@ data_banco %>%
 data_banco %>% 
   select( Tiempo_Servicio_seg ) %>% 
   boxplot()
+
+# Todas las columnas a excepcion de la columna Cajero
+
+data_banco %>% 
+  select( -Cajero ) %>% 
+  View()
+
+# Busqueda de columnas por sus nombres
+
+data_banco %>% 
+  select( contains('Tra') ) %>% View()
+
+data_banco %>% 
+  select( starts_with('S') ) %>% View()
+
+data_banco %>% 
+  select( matches('r?sa') ) %>% View()
+
+# Filtrado
+
+data_banco %>% 
+  filter( Sucursal == 62 ) %>% View()
+
+data_banco %>% 
+  filter( Sucursal == 62 & Tiempo_Servicio_seg >= 120 ) %>% View()
+
+data_banco %>% 
+  filter( Sucursal == 62 & Tiempo_Servicio_seg >= 120 & Satisfaccion == 'Muy Bueno' ) %>% View()
+
+# Consultar la correlacion del tiempo de servicio y el monto
+# Teniendo en cuenta una sucursal en particular (85).
+
+data_banco %>% 
+  filter( Sucursal == 85 ) %$% 
+  cor( Tiempo_Servicio_seg, as.numeric( Monto )  )
+
+# Ordenamiento de datos
+
+data_banco %>% arrange( Satisfaccion ) %>% View()
+
+data_banco %>% arrange( Satisfaccion, desc(Tiempo_Servicio_seg) ) %>% View()
+
+# Crear una nueva columna
+
+data_banco %>% mutate(Tiempo_Servicio_min=Tiempo_Servicio_seg/60) %>% View()
+data_banco <- data_banco %>% mutate(Tiempo_Servicio_min=as.integer(Tiempo_Servicio_seg/60))
+str(data_banco)
+
+data_banco <- data_banco %>% 
+  mutate( Monto = str_replace(Monto, pattern = ',', replacement = '.') ) %>% 
+  mutate( Sucursal = as.character(Sucursal),
+          Cajero = as.character(Cajero),
+          Satisfaccion = parse_factor(Satisfaccion, 
+                                      levels = c('Muy Malo', 'Malo', 'Regular', 'Bueno', 'Muy Bueno'),
+                                      ordered = T), 
+          Monto = parse_number(Monto, locale = locale(decimal_mark = '.')))
+
+str(data_banco)
+
+# Estadistica Descriptiva
+
+
+
+
+
+
 
